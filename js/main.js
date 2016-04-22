c6a3c0681767b4bfc82fa0b65019be2e1c6d081c
Christopher Lee <zerosxl@gmail.com>
2016-04-22 14:07:20 -0400

    update skills

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 	$(".navbar-nav li a").click(function(event) {
-    	$(".navbar-collapse").collapse('hide');
+    $(".navbar-collapse").collapse('hide');
 	});
 	$('a[href*=#]').on('click', function(event){
-    	event.preventDefault();
-    	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-  	});
-  	$(".proj-pic").hover(
-        function() { $(this).addClass("Hover"); },
-        function() { $(this).removeClass("Hover"); }
-    );
+    if (event.target.className == "left carousel-control" || event.target.className == "right carousel-control") {
+    } else {
+      event.preventDefault();
+      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    }
+  });
+  $(".proj-pic").hover(
+    function() { $(this).addClass("Hover"); },
+    function() { $(this).removeClass("Hover"); }
+  );
 });
